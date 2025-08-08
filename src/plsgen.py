1fdc234753a4db013bb868d264ecc9caabafa061
João Paulo Darela Filho <darelafilho@gmail.com>
2025-08-09 01:56:55 +0200

    add more noise to leaf N

--- src/plsgen.py
+++ src/plsgen.py
@@ -55,13 +55,8 @@ description = """ Generate and save a table of plant life strategies for CAETE.
                   (can be nested folders like foo/bar/zip).The parameter NUMBER
                   is the number of PLS given by the -n (--number) flag. Optionally you can import the
                   table_gen function defined in this script and use it in your own code.
-                  In a python script, you can use the function as follows:
-
-                  from plsgen import table_gen
-
-                  table = table_gen(NUMBER)
-
-                 The created object table is a numpy array with axis (NTRAITS, NPLS).
+                  In a python script, you can import the table_gen function to generate a np.ndarray
+                  with the PLS traits. The table will have the shape (NTRAITS, NPLS) where NTRAITS is the number of traits.
                  """
 
 parser = argparse.ArgumentParser(
@@ -245,7 +240,7 @@ def nutrient_ratios_combinations_reich(NPLS, alloc):
     # 1. Leaf N from Reich et al. (1997)
     leaf_longevity_months = alloc[:, 0] * 12.0  # tleaf (years) to months
     N_leaf_mg_g = 42.7 * (leaf_longevity_months ** -0.32)
-    N_leaf_g_g = N_leaf_mg_g / 1000.0 + np.random.normal(0, 0.001, NPLS)  # Convert to g/g and add noise
+    N_leaf_g_g = N_leaf_mg_g / 1000.0 + np.random.normal(0, 0.002, NPLS)  # Convert to g/g and add noise
     # N_leaf_g_g = np.clip(N_leaf_g_g, nr["leaf_n2c"]["min"], nr["leaf_n2c"]["max"])
 
     # 2. Leaf P from fixed N:P ratio (Reich & Oleksyn 2004)
